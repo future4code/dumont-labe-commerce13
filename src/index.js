6f067da5cec285594db4b96902bff71f593b420d
Roberthy Lima <62851647+robex99@users.noreply.github.com>
2020-10-27 10:22:10 -0300

    Revert "PR de Correção"

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
-import {ProductProvider} from './context'
 import * as serviceWorker from './serviceWorker';
 
-
 ReactDOM.render(
-  <ProductProvider>
-    <Router>
-      <App />
-    </Router>
-  </ProductProvider>
-  ,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
